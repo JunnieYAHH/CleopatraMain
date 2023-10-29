ba2e36c5d6bfefa099b57e65c14f601f4d942dce
itsk2 <codonkellyn3@gmail.com>
2023-10-30 00:15:41 +0800

    ProductController-getSingleProduct & routes

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { createProduct, getProducts } = require('../controllers/productController')
+const { createProduct, getProducts, getSingleProduct } = require('../controllers/productController')
 
 router.route('/products').get(getProducts);
-
+router.route('/product/:id').get(getSingleProduct);
 router.route('/products/create').post(createProduct);
 
 module.exports = router;
