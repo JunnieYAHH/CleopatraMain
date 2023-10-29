89e1b7d55d16fd9b3cbcd402d409a7f5fb10965c
itsk2 <codonkellyn3@gmail.com>
2023-10-30 01:16:27 +0800

    DeleteProductsController&Router

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const { createProduct, getProducts, getSingleProduct } = require('../controllers/productController')
+const { createProduct, 
+    getProducts, 
+    getSingleProduct, 
+    updateProduct, 
+    deleteProduct
+
+} = require('../controllers/productController')
 
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
-router.route('/products/create').post(createProduct);
+router.route('/admin/products/create').post(createProduct);
+
+router.route('/admin/product/:id')
+              .put(updateProduct)
+              .delete(deleteProduct);
 
 module.exports = router;
