1562c1c9e0b9cc758820ae7cd9e8f6a83cf6b539
JunnieYAHH <gerelitopuyos02@gmail.com>
2023-10-30 12:35:54 +0800

    Modified

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -58,5 +58,21 @@ exports.updateProduct = async (req, res, next) => {
         success: true,
         product
     })
+}
 
+//Delete Product =>  /api/v1/admin/product/id
+
+exports.deleteProduct = async (req, res, next) => {
+    const product =  await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }    
+    res.status(200).json({
+        success: true,
+        message: 'Product is Deleted.'
+    })
+    
 }
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { createProduct, getProducts, getSingleProduct, updateProduct} = require('../controllers/productController')
+const { createProduct, getProducts, getSingleProduct, updateProduct, deleteProduct} = require('../controllers/productController')
 
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 router.route('/admin/products/create').post(createProduct);
-router.route('/admin/product/:id').put(updateProduct);
+router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct);
 
 module.exports = router;
