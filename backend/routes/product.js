e9d60a05aedae3a1291a1389de4d12baa8274d62
Dottyyyyy <Orekihoutarou126@gmail.com>
2023-10-31 14:10:56 +0800

    authUser

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const { createProduct, getProducts, getSingleProduct, updateProduct, deleteProduct} = require('../controllers/productController')
 
+const { isAuthenticatedUser } = require('../middlewares/auth');
+
+
 router.route('/products').get(getProducts);
 router.route('/product/:id').get(getSingleProduct);
 router.route('/admin/product/create').post(createProduct);
