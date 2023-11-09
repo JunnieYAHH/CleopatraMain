7a125f4281b35a75c2089570d2e53d15566c6ff3
JunnieYAHH <gerelitopuyos02@gmail.com>
2023-11-09 10:14:23 +0800

    DesignFrontend

--- frontend/src/components/products/Product.js
+++ frontend/src/components/products/Product.js
@@ -4,23 +4,37 @@ import { Link } from 'react-router-dom'
 const Product = ({ product }) => {
     return (
         <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-            <div className="card p-3 rounded">
+            <div className="card p-3 rounded shadow">
                 <img
-                    className="card-img-top mx-auto"
+                    className="card-img-top mx-auto img-fluid"
                     src={product.images[0].url}
+                    alt={product.name}
                 />
                 <div className="card-body d-flex flex-column">
                     <h5 className="card-title">
-                        <Link to={`/product/${product._id}`} >{product.name}</Link>
+                        <Link to={`/product/${product._id}`} className="product-link">
+                            {product.name}
+                        </Link>
                     </h5>
                     <div className="ratings mt-auto">
                         <div className="rating-outer">
-                            <div className="rating-inner" style={{ width: `${(product.ratings / 5) * 100}%` }}></div>
+                            <div
+                                className="rating-inner"
+                                style={{ width: `${(product.ratings / 5) * 100}%` }}
+                            ></div>
                         </div>
-                        <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
+                        <span className="text-muted">({product.numOfReviews} Reviews)</span>
                     </div>
-                    <p className="card-text">${product.price}</p>
-                    <Link to={`/product/${product._id}`} id="view_btn" className="btn btn-block">View Details</Link>
+                    <p className="card-text font-weight-bold">
+                        Price: $ {product.price}
+                    </p>
+                    <Link
+                        to={`/product/${product._id}`}
+                        id="view_btn"
+                        className="btn btn-primary btn-block"
+                    >
+                        View Details
+                    </Link>
                 </div>
             </div>
         </div>
